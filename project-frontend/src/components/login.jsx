abda4cfc382bc82b2a8e0ccc6277957f358460f7
ulvakirtan <ulvakirtan@gmail.com>
2026-03-10 13:56:13 +0530

    second commit

---
+++ project-frontend/src/components/login.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="login">
+      <h1>Login</h1>
+      <form>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
